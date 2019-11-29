6559a8eced989a5c78c9da5ca92b65971e811f66
Marco Judt <ic18b039@technikum-wien.at>
2019-11-29 21:19:22 +0100

    -p argument

--- simple_message_server.c
+++ simple_message_server.c
@@ -9,7 +9,6 @@
  * @date 11/25/2019
  * @version 1.0
  *
- * @TODO parse arguments with getopt
  */
 
 /*
@@ -21,7 +20,6 @@
 #include <string.h>
 #include <unistd.h>
 #include <getopt.h>
-
 
 /*
  * --------------------------------------------------------------- defines --
@@ -40,12 +38,19 @@ const char *prog_name = "";
  * ------------------------------------------------------------- prototypes --
  */
 static void usage(void);
-
+static void error_exit (int error, char* message, int socket_fd);
 
 /*
  * ------------------------------------------------------------- functions --
  */
 
+/**
+* \brief
+*
+*
+\param
+\return
+*/
 int main (const int argc, char* const argv[])
 {
     prog_name = argv[0];
@@ -53,23 +58,31 @@ int main (const int argc, char* const argv[])
     if (argc < 2)
     {
         usage();
-        // TODO: Close Sockets
         return EXIT_FAILURE;
     }
 
     // Parse command line arguments with getopt
-    int opt;
+    int opt = 0;
+    long int port = 0;
+    char *portstring_ptr = NULL;
     static struct option long_options[] = {
-            {"port",     required_argument, 0,  'p' },
+            {"port",  required_argument, 0,  'p' },
             {"help",  no_argument,       0,  'h' },
             {NULL,  0, 0,  0 }
     };
 
-    while ((opt = getopt_long(argc, argv, "p:h",long_options, NULL)) != -1) {
+    while ((opt = getopt_long(argc, argv, "p:h", long_options, NULL)) != -1) {
         switch (opt) {
             case 'p':
-                // TODO: -p
-                printf("-p is in work ...\n");
+                errno = 0;
+                port = strtol(optarg, &portstring_ptr, 10);
+                if (errno != 0) {
+                    error_exit(errno, "strtol() failed", 0);
+                }
+                if(port <= 0 || port >= 65536 || *portstring_ptr != '\0') {
+                    error_exit(0, "port is invalid", 0);
+                }
+                printf("You have port: %ld\n", port);
                 break;
             case 'h':
                 usage();
@@ -100,9 +113,29 @@ static void usage(void)
                "\t-p, --port <port>\n"
                "\t-h, --help\n" , prog_name) < 0)
     {
-        fprintf(stderr, "%s: printf(): %s\n", prog_name, strerror(errno));
-        exit(EXIT_FAILURE);
+        error_exit(errno, "printf() failed", 0);
     }
+}
+
+/**
+* \brief
+*
+*
+\param
+\return void
+*/
+static void error_exit (int error, char* message, int socket_fd) {
+    if (socket_fd != 0) {
+        close(socket_fd);
+    }
+
+    if (error != 0) {
+        fprintf(stderr, "%s: %s: %s\n", prog_name, message, strerror(error));
+    }
+    else {
+        fprintf(stderr, "%s: %s\n", prog_name, message);
+    }
+    exit(EXIT_FAILURE);
 }
 
 /*
